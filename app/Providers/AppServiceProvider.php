62f1b84857c914689fa60112d02828bb4e335c66
Glendon Small <gsmall@sonovate.com>
2018-10-28 14:08:55 +0000

    Added  API support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,16 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 class AppServiceProvider extends ServiceProvider
 {
     /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+
+    /**
      * Bootstrap any application services.
      *
      * @return void
