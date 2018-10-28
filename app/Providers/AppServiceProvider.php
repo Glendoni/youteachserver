7f9c7549c82178200df4ec725002a1501e9c9348
Glendon Small <gsmall@sonovate.com>
2018-10-28 14:16:57 +0000

    Removed Auth path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         //
        $this->registerPolicies();
         Passport::routes();
-        Passport::loadKeysFrom('/secret-keys/oauth');
+       
     }
 
     /**
